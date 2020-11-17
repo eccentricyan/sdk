214c520c66b5e69c46b78c4b41f90409965c08a9
eccentricyan <eccentricyan@gmail.com>
2020-11-17 10:02:30 +0900

    fix

---
+++ sdk/SDKPrinter.swift
@@ -0,0 +1,14 @@
+//
+//  SDKPrinter.swift
+//  sdk
+//
+//  Created by seki on 2020/11/17.
+//
+
+import Foundation
+
+public class SDKPrinter: NSObject {
+    public func show() {
+        print("Sally().sallyMethod is called !!")
+    }
+}
